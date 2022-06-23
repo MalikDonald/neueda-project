d44834716a04b338c30b45f244ec3c10419ec8fc
Andrew Roberts <andrew.roberts@citi.com>
2022-06-23 20:29:10 +0000

    Completed through '2. Display a List' on Angular tutorial and added all team members information.

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,6 +1,7 @@
 export interface Hero {
     id: string;
-    name: string;
+    fname: string;
+    lname: string;
     email: string;
     devTeam: string;
 }
--- src/app/teamMembers.ts
+++ src/app/teamMembers.ts
@@ -1,6 +1,10 @@
 import { Hero } from './hero';
 
 export const TEAM: Hero[] = [
-    { id: "AR48368", name: "Andrew Roberts", email: "andrew.roberts@citi.com", devTeam: "Security Services Technology" },
-    { id: "CY12345", name: "Calvin Yang", email: "calvin.yang@citi.com", devTeam: "Security Services Technology" }
+    { id: "AR48368", fname: "Andrew", lname: "Roberts", email: "andrew.roberts@citi.com", devTeam: "Security Services Technology" },
+    { id: "CY47989", fname: "Calvin", lname: "Yang", email: "calvin.yang@citi.com", devTeam: "Security Services Technology" },
+    { id: "CS49623", fname: "Claire", lname: "Speight", email: "claire.speight@citi.com", devTeam: "Trade Technology" },
+    { id: "AD47858", fname: "Akin", lname: "Donald", email: "akin.malik.donald@citi.com", devTeam: "Production Support" },
+    { id: "AW47938", fname: "Arianna", lname: "Walters", email: "arianna.walters@citi.com", devTeam: "Equities & Prime Technology" },
+    { id: "AB47750", fname: "Angel", lname: "Boakye", email: "angel.afuah.boakye@citi.com", devTeam: "Production Support" }
 ]
